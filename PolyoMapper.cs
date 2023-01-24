2ce3b9733ed6944d090dc97eef208bf983dde909
Kazukima Miller <kazukimamiller@gmail.com>
2023-01-23 20:06:26 -0800

    Renamed some stuff, added boxes for adding cols/rows

---
+++ PolyoMapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PolyoMapper
+{
+    public partial class PolyoMapper : Form
+    {
+        public PolyoMapper()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btn_addColumns_Click(object sender, EventArgs e)
+        {
+             for (int i = 0; i < int.Parse(numBx_addColumns.Text); i++)
+            {
+                dataGridView1.Columns.Add("columnName", "Display Name");
+            }
+        }
+
+        private void btn_addRows_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < int.Parse(numBx_addRows.Text); i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PolyoMapper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PolyoMapper());
         }
     }
 }
